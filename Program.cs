28ca9e75e38d3e1fca04d867da4b7cf28ae05c6f
BorvD <xenomorph_85@hotmail.com>
2025-10-01 12:59:03 +0200

    Övning 1 är klar

--- Program.cs
+++ Program.cs
@@ -4,22 +4,30 @@ using System.Diagnostics.Metrics;
 namespace ÖVNING_1___Lön_efter_skatt
 {
 
-
     internal class Program
     {
         static void Main(string[] args)
         {
-                //Skapa en Console Application.
-                //Skapa en klass SalaryCalculator med publika variabler eller properties för:
+            //Skapa en Console Application.
+            //Skapa en klass SalaryCalculator med publika variabler eller properties för:
 
-                //Bruttolön
-                //Skattesats(t.ex. 30 %)
+            //Bruttolön
+            //Skattesats(t.ex. 30 %)
 
-                //Skapa en metod CalculateNetSalary() som returnerar nettolönen(lön efter skatt).
-                //I Program.cs:
+            //Skapa en metod CalculateNetSalary() som returnerar nettolönen(lön efter skatt).
+            //I Program.cs:
 
-                //Be användaren mata in bruttolön och skattesats via Console.ReadLine()
-                //Skapa ett objekt av klassen och skriv ut nettolönen med Console.WriteLine()
+            //Be användaren mata in bruttolön och skattesats via Console.ReadLine()
+            //Skapa ett objekt av klassen och skriv ut nettolönen med Console.WriteLine()
+
+            Console.WriteLine("Choose what program you wanna run bu typing its number");
+            Console.WriteLine("1: Calculate your salary after taxes");
+            string mainMenu = Console.ReadLine();
+            if (mainMenu == "1")
+            {
+                SalaryCalculator salaryCalculator = new SalaryCalculator();
+                salaryCalculator.CalculateNetSalary();
+            }
         }
     }
 }
--- SalaryCalculator.cs
+++ SalaryCalculator.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ÖVNING_1___Lön_efter_skatt
 {
+    //Skapa en Console Application.
+    //Skapa en klass SalaryCalculator med publika variabler eller properties för:
+
+    //Bruttolön
+    //Skattesats(t.ex. 30 %)
+
+    //Skapa en metod CalculateNetSalary() som returnerar nettolönen(lön efter skatt).
+    //I Program.cs:
+
+    //Be användaren mata in bruttolön och skattesats via Console.ReadLine()
+    //Skapa ett objekt av klassen och skriv ut nettolönen med Console.WriteLine()
     public class SalaryCalculator
     {
+        public void CalculateNetSalary()
+        {
+            Console.WriteLine("Ange din bruttolön");
+            int brutto = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Ange din skattesats i procent (bara siffror)");
+            double percent = Convert.ToInt32(Console.ReadLine());
+            double netto = (percent / 100) * brutto;
+
+            Console.WriteLine($"Din bruttolön: {brutto}  Din skattesats: {percent}  Din Nettolön: {netto}");
+        }
     }
 }
