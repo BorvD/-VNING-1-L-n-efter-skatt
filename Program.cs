a28b812deec6bc98e3c350747c58ed2bb57eef0b
BorvD <xenomorph_85@hotmail.com>
2025-10-01 12:11:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace ÖVNING_1___Lön_efter_skatt
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //Skapa en Console Application.
+                //Skapa en klass SalaryCalculator med publika variabler eller properties för:
+
+                //Bruttolön
+                //Skattesats(t.ex. 30 %)
+
+                //Skapa en metod CalculateNetSalary() som returnerar nettolönen(lön efter skatt).
+                //I Program.cs:
+
+                //Be användaren mata in bruttolön och skattesats via Console.ReadLine()
+                //Skapa ett objekt av klassen och skriv ut nettolönen med Console.WriteLine()
+        }
+    }
+}
---
+++ SalaryCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖVNING_1___Lön_efter_skatt
+{
+    public class SalaryCalculator
+    {
+
+    }
+}
